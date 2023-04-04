e8b0ef55d3021dcbedee6af21ad49aa375ca2458
avinash kumar <avi064448@gmail.com>
2023-04-04 15:25:38 +0530

    added1

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -26,16 +26,19 @@ userRouter.patch("/calculator",async(req,res)=>{
     i=i/100;
     try{
         const F= p*[(((1+i)^n)-1)/i];
-        const TI = p * n;
+        const TI = p*n;
         const TG = F - TI;
         await UserModel.findByIdAndUpdate({_id:userID},{total_Investment_Amount:TI,
             total_Interest_Gained:TG,
-            total_Maturity_Value:F ,});
+            total_Maturity_Value:F});
         res.send(user)
     }catch(err){
         res.send({"msg":"cannot Calculate","error":err.message})
     }
 })
+
+
+
 
 // below code can be used to Logout Account by users---------------->
 userRouter.patch("/logout",async(req,res)=>{
